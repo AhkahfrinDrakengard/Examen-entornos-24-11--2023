c201c84f033c3af54d55e8e50c1cbaf794faf3f0
AhkahfrinDrakengard <amartinezlopez@fp.mercedarias.es>
2023-11-24 19:06:42 +0100

    Create App-Maven.java

---
+++ App-Maven.java
@@ -0,0 +1,25 @@
+package com.mgh.edd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven y Ant tienen muchas diferencias," );
+        System.out.println( "pero als principales difetencias entre estas dos herramientas" );
+        System.out.println( "son la felxivilidad y la cantidd de proyectos que se pueden intervenir a la vez." );
+        System.out.println( "Ant es muy felxible pero solo puede intervenir un proyecto" );
+        System.out.println( "mientras que Maven puede intervenir varios proyectos a la vez, pero este no es tan flexible como Ant." );
+        System.out.println( "Una vez aclaradas la disferencias entre Maven y Ant, tenemos que hablar de Gradle." );
+        System.out.println( "Gradle es una herramienta no solo muy flexible, ya que como en Ant nos permite manejar " );
+        System.out.println( "los proyectos como quermos, sino que tambien nos premite interactiar con parios proyectos" );
+        System.out.println( "a la vez como Maven." );
+        System.out.println( "Una vez dicho todo esto, expondre cual es la herramienta que utilizaria:" );
+        System.out.println( "Esta seria Gradle, ya que podemos editar varios proyectos a la vez para poder tener un" );
+        System.out.println( "desarrollo mas rapido y organico y, ademas, podemos hacer los proyectos con los estandares que consideremos" );
+        System.out.println( "gracias a su flexibilidad" );
+    }
+}
